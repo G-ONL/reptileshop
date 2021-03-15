6ba6bb1f43ec95c41941bb3e219fbbce8042011d
kimjaeyun <je941004@naver.com>
2021-03-15 21:31:57 +0900

    Initial commit

---
+++ src/main/java/com/woopaloopa/reptileshop/ReptileShopApplication.java
@@ -0,0 +1,13 @@
+package com.woopaloopa.reptileshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReptileShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReptileShopApplication.class, args);
+	}
+
+}
